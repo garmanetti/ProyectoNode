9996bf37d30a98edc0db9f76e72469a9f45395b8
garmanetti <gaston.armanetti@gmail.com>
2019-11-28 18:32:56 -0300

    Alta/Baja usuario

--- src/routes/v1/user.routes.js
+++ src/routes/v1/user.routes.js
@@ -1,34 +1,73 @@
-const { hash } = require("bcrypt");
-const { sign } = require("jsonwebtoken");
-const { User } = require("../../models");
-const { JWT_SECRET } = require("../../constants");
-const checkJwt = require("express-jwt");
+const express = require('express');
 
-module.exports = router => {
-  router.post("/users", (req, res) => {
-    hash(req.body.password, 10)
-      .then(hash => {
-        return User.create({
-          email: req.body.email,
-          hash
-        });
-      })
-      .then(doc => {
-        const token = sign({ id: doc._id }, JWT_SECRET);
-        res.json({
-          user: doc.toObject({
-            transform(doc, ret, options) {
-              delete ret.hash;
-              return ret;
-            }
-          }),
-          token
-        });
-      })
-      .catch(error => {
-        res.send(error);
-      });
+const { json } = require('body-parser');
+const { sign, decode } = require('jsonwebtoken');
+const { compare, hash } = require('bcrypt');
+const checkJwt = require('express-jwt');
+
+const User = require("../../models/user.model");
+
+const { JWT_SECRET } = require("../../constants");
+
+module.exports = (app) => {
+
+    // ALTA
+    app.post('/', json(), (req, res) => {
+      const userBody = req.body;
+
+      hash(userBody.password, 10)
+          .then((hash) => {
+              return User.create({
+                  username:   userBody.username,
+                  email:      userBody.email,
+                  password:   hash
+              })
+          })
+          .then((created_user) => {
+              const token = sign({}, JWT_SECRET);
+
+              res.json({
+                  user: created_user,
+                  token
+              });
+          })
+          .catch((error) => {
+              if(error.code === 11000){
+                  res
+                  .status(401)
+                  .json({
+                      message: ('El usuario con el mail "' + error.keyValue.email + '" ya existe en el sistema')
+                  })
+              }
+              
+              res
+              .status(400)
+              .json({
+                  error: error.message
+              });
+          });
   });
 
-  return router;
+  //BAJA
+  app.delete('/', json(),checkJwt({ secret: JWT_SECRET }), (req, res) => {
+    const userBody = req.body;
+
+    User.findByIdAndDelete(userBody.username)
+            .then((doc) => {
+                res
+                    .status(200)
+                    .json({
+                        user: doc
+                    })
+            })
+            .catch((err) => {
+                res
+                    .status(400)
+                    .json({
+                        error: err.json
+                    })
+            })
+});
+
+  return app;
 };
